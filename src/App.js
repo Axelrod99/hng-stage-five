371a75ba17ce389f663b69bcfec46b27ab180c5e
Lawal Taofik <layomide81@gmail.com>
2023-09-30 04:13:20 +0100

    mod app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import "./App.css";
 import LandingPage from "./pages/LandingPage/landingPage";
 import HomePage from "../src/pages/landing-page/Home-page";
-// import Home from "./pages/Home/Home";
-import VideoPage from "./pages/VideoPage/VideoPage";
+
 
 function App() {
   return (<>
-  {/* <LandingPage /> */}
-    {/* <Home /> */}
-    <VideoPage />
+  <LandingPage />
+   
   </>
   );
 }
