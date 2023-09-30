73815c1ea2f6d40c9ef52e49be99c87f3e40a19d
Lawal Taofik <layomide81@gmail.com>
2023-09-30 04:12:11 +0100

    video page component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import "./App.css";
 import LandingPage from "./pages/LandingPage/landingPage";
 import HomePage from "../src/pages/landing-page/Home-page";
+// import Home from "./pages/Home/Home";
+import VideoPage from "./pages/VideoPage/VideoPage";
 
 function App() {
-  return <LandingPage />;
+  return (<>
+  {/* <LandingPage /> */}
+    {/* <Home /> */}
+    <VideoPage />
+  </>
+  );
 }
 
 export default App;
